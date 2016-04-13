a7f0fa92ddfb2085d0a17d2ffd2e6d456f61943e
Joe Corcoran <joecorcoran@gmail.com>
2016-04-13 16:58:29 +0200

    Add static asset handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use] extern crate nickel;
 extern crate nickel_mustache;
 
-use nickel::{Nickel, HttpRouter};
+use nickel::{Nickel, HttpRouter, Mountable, StaticFilesHandler};
 use nickel::status::StatusCode;
 use nickel_mustache::Render;
 use std::collections::HashMap;
@@ -13,15 +13,18 @@ fn main() {
     let mut server = Nickel::new();
     let (root, bind, port) = opts::compile();
 
+    let root_copy = root.clone();
     server.get("/:page", middleware! { |request, response|
         let page = request.param("page").unwrap_or("missing");
-        let path = sitepath::page_path(&root, &page);
+        let path = sitepath::page_path(&root_copy, &page);
         if !path.is_file() {
             return response.error(StatusCode::NotFound, "Page not found");
         }
         let data: HashMap<&str, &str> = HashMap::new();
-        return response.render_with_layout(path, sitepath::layout_path(&root), &data)
+        return response.render_with_layout(path, sitepath::layout_path(&root_copy), &data)
     });
+
+    server.mount("/static/", StaticFilesHandler::new(sitepath::static_path(&root)));
 
     let mut socket_addr = String::new();
     socket_addr.push_str(&bind);
--- src/sitepath.rs
+++ src/sitepath.rs
@@ -16,3 +16,10 @@ pub fn layout_path(root: &str) -> PathBuf {
     path.set_extension("mustache");
     path
 }
+
+pub fn static_path(root: &str) -> PathBuf {
+    let mut path = PathBuf::new();
+    path.push(root);
+    path.push("static");
+    path
+}
